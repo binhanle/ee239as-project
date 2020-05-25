4a412c50ca7233bff910ad6ebb97fc6cf604dbdf
An Le <38597340+binhanle@users.noreply.github.com>
2020-05-25 12:24:39 -0700

    use joblib to save memory buffer to file

--- checkpoint.py
+++ checkpoint.py
@@ -1,20 +1,23 @@
 import torch
+import joblib
+
+MEMORY_EXT = ".mem"
 
 def save_checkpoint(mem_buffer, progress, dqn_online, dqn_target, optimizer, filename):
     checkpoint = {
-        "mem_buffer": mem_buffer,
         "progress": progress,
         "dqn_online": dqn_online.state_dict(),
         "dqn_target": dqn_target.state_dict(),
         "optimizer": optimizer.state_dict()
     }
     torch.save(checkpoint, filename)
+    joblib.dump(mem_buffer, filename + MEMORY_EXT)
 
 def load_checkpoint(dqn_online, dqn_target, optimizer, filename):
+    mem_buffer = joblib.load(filename + MEMORY_EXT)
     checkpoint = torch.load(filename)
-    mem_buffer = checkpoint['mem_buffer']
-    progress = checkpoint['progress']
-    dqn_online.load_state_dict(checkpoint['dqn_online'])
-    dqn_target.load_state_dict(checkpoint['dqn_target'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    progress = checkpoint["progress"]
+    dqn_online.load_state_dict(checkpoint["dqn_online"])
+    dqn_target.load_state_dict(checkpoint["dqn_target"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
     return mem_buffer, progress
